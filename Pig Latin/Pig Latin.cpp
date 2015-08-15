600446f221a6d8f6981def0c7a3b000085d51cc3
Ameji <amejinight@gmail.com>
2015-08-14 22:02:48 -0500

    Pig Latin commented
    Added comments to the Pig Latin.cpp file.

--- Pig Latin/Pig Latin.cpp
+++ Pig Latin/Pig Latin.cpp
@@ -1,24 +1,26 @@
 // Pig Latin.cpp : Defines the entry point for the console application.
-//
+// This is the main source file (Source = .cpp) that the compiler will run. 
+// This is decided by the source file with the main() function.
 
-#include "stdafx.h"
+#include "stdafx.h" // A header file that saves space in the file. 
+// Read stdafx.h for more information.
 
-int main()
+int main() // The main function, the first thing the compiler will run.
 {
-	cout << "String: " << flush;
-	string pigLatin;
-	getline(cin, pigLatin);
+	/* Simple command to print something out. */ cout << "String: " << flush; // Flush controls buffer, which stops the printing of a huge array.
+	string pigLatin; // Defining the string
+	getline(cin, pigLatin); // Storing the user's input
 
-	pigLatin += '-';
+	pigLatin += '-'; // Adding "-'first letter'ay"
 	pigLatin += pigLatin[0];
 	pigLatin += "ay";
 
-	for (int i = 1; pigLatin[i] != 0; i++)
+	for (int i = 1; pigLatin[i] != 0; i++) // For loop to print out the values of the string's char array.
 	{
 		cout << pigLatin[i];
 	}
 
 	cout << endl;
-	system("PAUSE");
+	system("PAUSE"); 
 }
 

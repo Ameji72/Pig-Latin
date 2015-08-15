d3c2385c0cedef3d59149ee0da2b9512b8ed66a6
Ameji <amejinight@gmail.com>
2015-08-14 22:04:39 -0500

    stdafx.h Commented
    Added comments to stdafx.h

--- Pig Latin/stdafx.h
+++ Pig Latin/stdafx.h
@@ -1,7 +1,8 @@
 // stdafx.h : include file for standard system include files,
 // or project specific include files that are used frequently, but
 // are changed infrequently
-//
+// In simpler words, if the program required more files, the files wouldn't have this huge list at the top all the time. 
+// stdafx.h holds the information, so just by including it in your file, the file will have access to all of these.
 
 #pragma once
 
